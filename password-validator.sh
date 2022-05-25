369c4b141cf33283cd14fc72355c2141a565270d
Idan Horovitz <idanhh55@gmail.com>
2022-05-25 17:16:45 +0300

    Create password validation script

---
+++ password-validator.sh
@@ -0,0 +1,33 @@
+#!/bin/bash 
+password=$1 # Insert the first argument to password varible.
+
+# Set color as varibles.
+Green='\033[0;32m' 
+Red='\033[0;31m' 
+default='\033[0;0m'
+                
+# Check of the password isnt less than 10.
+if [[ ${#password} -lt 10 ]];
+then
+        echo -e "${Red}Password must be minimum of 10 characters ${default}"
+        exit 1
+fi
+
+# Check if the password inculdes alphabet and number.
+if ! [[ ($password =~ [[:alpha:]]) && ($password =~ [[:digit:]]) ]];
+then  
+        echo -e "${Red}Password must contain both alphabet and number ${default}"
+        exit 1
+fi
+
+# Check if the password includes both the small and capital case letters.
+if ! [[ ($password =~ [[:lower:]]) && ($password =~ [[:upper:]]) ]];
+then
+        echo -e "${Red}Passowrd must include both the small and capital case letters ${default}"
+        exit 1
+fi
+
+# In case the password passed all the tests, it will print the password marked with green color.
+echo -e "${Green}${password}${default}"
+
+exit 0
